ce480113418150bf77f46b7516ce4f3987555722
itsmeeAbhi <107909223+itsmeeAbhi@users.noreply.github.com>
2022-06-30 23:33:42 +0530

    Add files via upload

---
+++ JavaScript/Practice.js
@@ -0,0 +1,8 @@
+//alert("Connected");
+//var deposit = prompt("How much you want to deposit");
+//alert("You have deposited: " +deposit);
+
+var acceptInput = prompt("Enter the weight(pounds)");
+var convert = (acceptInput*0.454);
+alert("Weight in Kg is: " +convert);
+console.log("Conversion Completed");
---
+++ JavaScript/loop.js
@@ -0,0 +1,34 @@
+alert("Connected");
+
+/* var hot = false;
+var temp = prompt("Enter the temp");
+if(temp > 36){
+    hot = true;
+    console.log("High Temperature");
+}else if(temp<= 36 && temp>=30){
+    console.log("Normal Temperature");
+}else if(temp<30 && temp>=24){
+    console.log("Pleasent Temperature");
+}else if(temp<24 && temp>=19){
+    console.log("temperature decresing Turnoff Ac");
+}else{
+    console.log("Its very cold");
+} */
+
+/*var num = 0;
+while(num<11){
+    if(num%2 == 0){
+        console.log(num);
+    }
+    num = num+1;
+}
+for(var i=0; i< 5; i++){
+    console.log("Number is "+i);
+} */
+
+//var word  = "hello";
+var i = 0;
+while(i<5){
+    console.log("Hello");
+    i++;
+}
